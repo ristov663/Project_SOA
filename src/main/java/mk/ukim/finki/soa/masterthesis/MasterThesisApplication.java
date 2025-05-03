774f6d50b099790dae7299deec56c4a06e4153cc
Ефтимов Стефан <stefan.eftimov.1@students.finki.ukim.mk>
2025-05-03 08:36:31 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/soa/masterthesis/MasterThesisApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.soa.masterthesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/soa/masterthesis/MasterThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.soa.masterthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
